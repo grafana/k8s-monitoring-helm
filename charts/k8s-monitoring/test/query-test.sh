de91758046f93df9eeeb9d4d1681fb2c5bff7075
Pete Wall <pete.wall@grafana.com>
2023-11-28 17:11:54 -0600

    Enable Receivers for all telemetry data types and add prometheus remote_write receiver.
    Also enable TraceQL queries to be run by helm test
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- charts/k8s-monitoring/test/query-test.sh
+++ charts/k8s-monitoring/test/query-test.sh
@@ -1,39 +1,49 @@
 #!/bin/bash
-set -eo pipefail
 
 function metrics_query {
   echo "Running PromQL query: ${PROMETHEUS_URL}?query=${1}..."
   result=$(curl -skX POST -u "${PROMETHEUS_USER}:${PROMETHEUS_PASS}" "${PROMETHEUS_URL}" --data-urlencode "query=${1}")
-  status=$(echo $result | jq -r .status)
+  status=$(echo "${result}" | jq -r .status)
   if [ "${status}" != "success" ]; then
     echo "Query failed!"
-    echo "$result"
-    exit 1
+    echo "Response: ${result}"
+    return 1
   fi
 
-  resultCount=$(echo $result | jq '.data.result | length')
+  resultCount=$(echo "${result}" | jq '.data.result | length')
   if [ "${resultCount}" -eq 0 ]; then
     echo "Query returned no results"
-    echo "$result"
-    exit 1
+    echo "Result: ${result}"
+    return 1
   fi
 }
 
 function logs_query {
   echo "Running LogQL query: ${LOKI_URL}?query=${1}..."
   result=$(curl -s --get -H "X-Scope-OrgID:${LOKI_TENANTID}" -u "${LOKI_USER}:${LOKI_PASS}" "${LOKI_URL}" --data-urlencode "query=${1}")
-  status=$(echo $result | jq -r .status)
+  status=$(echo "${result}" | jq -r .status)
   if [ "${status}" != "success" ]; then
     echo "Query failed!"
-    echo "$result"
-    exit 1
+    echo "Response: ${result}"
+    return 1
   fi
 
-  resultCount=$(echo $result | jq '.data.result | length')
+  resultCount=$(echo "${result}" | jq '.data.result | length')
   if [ "${resultCount}" -eq 0 ]; then
     echo "Query returned no results"
-    echo "$result"
-    exit 1
+    echo "Result: ${result}"
+    return 1
+  fi
+}
+
+function traces_query {
+  echo "Running TraceQL query: ${TEMPO_URL}?q=${1}..."
+  result=$(curl -sk --get -u "${TEMPO_USER}:${TEMPO_PASS}" "${TEMPO_URL}" --data-urlencode "q=${1}")
+  resultCount=$(echo "${result}" | jq '.traces | length')
+  if [ "${resultCount}" -eq 0 ]; then
+    echo "Query returned no results"
+    echo "Result: ${result}"
+    return 1
   fi
 }
 
@@ -44,13 +54,23 @@ for i in $(seq 0 "${count}"); do
 
   case "${type}" in
     promql)
-      metrics_query "${query}"
+      if ! metrics_query "${query}"; then
+        exit 1
+      fi
       ;;
     logql)
-      logs_query "${query}"
+      if ! logs_query "${query}"; then
+        exit 1
+      fi
+      ;;
+    traceql)
+      if ! traces_query "${query}"; then
+        exit 1
+      fi
       ;;
     *)
       echo "Query type ${type} is not yet supported in this test"
+      exit 1
       ;;
   esac
 done
