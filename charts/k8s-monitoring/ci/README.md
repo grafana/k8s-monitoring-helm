1394ec246e8918fd90e0d69e935c046d75e59f2d
Pete Wall <pete.wall@grafana.com>
2023-12-19 08:34:43 -0600

    Add annotations to support https scheme (#297)
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    Co-authored-by: Stephen Lang <skl@users.noreply.github.com>

--- charts/k8s-monitoring/ci/README.md
+++ charts/k8s-monitoring/ci/README.md
@@ -4,6 +4,35 @@ Any files that are named `*-values.yaml` will be used for testing.
 
 * `ci-values.yaml`
   * Default tests
+  * Control plane metrics
   * Extra test queries
-* `otlp-metrics-service-values.yaml`
-  * Tests the OTLP protocol for Prometheus
+* `ci-2-values.yaml`
+  * Sending metrics via OTLP HTTP
+  * Sending traces via OTLP HTTP
+  * Filtering pod logs by namespace
+  * Custom test queries, including LogQL and TraceQL queries
+* `ci-integrations-values.yaml`
+  * Loading integrations via modules
+  * Loading integrations via Pod annotations (and over https)
+  * Loading integrations via Service annotations
+
+## Architecture
+
+The CI/CD test environment is deployed out like this:
+
+![Deployment architecture](architecture.png "Deployment architecture")
+
+* Telemetry Sources
+  * Cert Manager, used for testing discovery using service annotations
+  * Grafana Agent, used for pushing metrics and traces using receivers
+  * MySQL, used for testing module integrations
+  * Prometheus, used for testing discovery using pod annotations and https
+  * The rest of Kubernetes
+* Subject under test
+  * k8s-monitoring helm chart, which deploys its own set of Grafana Agents
+* Data storage
+  * Prometheus
+  * Loki
+  * Tempo
+* Data visualization
+  * Grafana
--- scripts/setup-local-test-cluster.sh
+++ scripts/setup-local-test-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+CLUSTER_NAME="k8s-mon-test-cluster"
 
 CLUSTER_CONFIG="./.github/configs/cluster-config.yaml"
 PROMETHEUS_VALUES="./.github/configs/prometheus.yaml"
@@ -10,8 +10,12 @@ GRAFANA_VALUES="./.github/configs/grafana.yaml"
 SECRETGEN_CONTROLLER_MANIFEST=https://github.com/carvel-dev/secretgen-controller/releases/latest/download/release.yml
 CERTIFICATES_MANIFEST="./.github/configs/certificates.yaml"
 
-echo "Creating cluster..."
-kind create cluster --config "${CLUSTER_CONFIG}" --name k8s-mon-test-cluster
+if ! kind get nodes --name "${CLUSTER_NAME}" > /dev/null 2>&1; then
+  echo "Creating cluster..."
+  kind create cluster --config "${CLUSTER_CONFIG}" --name "${CLUSTER_NAME}"
+fi
+
+set -e
 
 echo "Creating SSL Certs and secrets..."
 kubectl apply -f "${SECRETGEN_CONTROLLER_MANIFEST}"
@@ -19,13 +23,13 @@ kubectl apply -f "${CERTIFICATES_MANIFEST}"
 kubectl apply -f "${CREDENTIALS}"
 
 echo "Deploying Prometheus..."
-helm install prometheus prometheus-community/prometheus -f "${PROMETHEUS_VALUES}" -n prometheus --create-namespace --wait
+helm upgrade --install prometheus prometheus-community/prometheus -f "${PROMETHEUS_VALUES}" -n prometheus --create-namespace --wait
 
 echo "Deploying Loki..."
-helm install loki grafana/loki -f "${LOKI_VALUES}" -n loki --create-namespace --wait
+helm upgrade --install loki grafana/loki -f "${LOKI_VALUES}" -n loki --create-namespace --wait
 
 echo "Deploying Tempo..."
-helm install tempo grafana/tempo -n tempo --create-namespace --wait
+helm upgrade --install tempo grafana/tempo -n tempo --create-namespace --wait
 
 echo "Deploying Grafana..."
-helm install grafana grafana/grafana -f "${GRAFANA_VALUES}" -n grafana --create-namespace --wait
+helm upgrade --install grafana grafana/grafana -f "${GRAFANA_VALUES}" -n grafana --create-namespace --wait
