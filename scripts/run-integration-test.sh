fd2b44280798ac2f72203ce0ba74595566e9d506
Pete Wall <pete.wall@grafana.com>
2024-11-27 20:51:13 -0600

    Update integration test scripts to work more generically (#918)
    * Update integration test scripts to work more generically
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Fix tests
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Cluster config needs to have the path pre-pended
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * GitHub Actions get the cluster config differently
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Need to match release name
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Make the control plane cluster two nodes, set the test cluster name, and fix yaml lint checks
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Shorten the cluster name
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Go back to the default cluster name
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Maybe this will work
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Set cluster name
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Need to clear out these variables otherwise they'll affect the next deployment
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Convert auth integration test to use deployments and not prerequisites
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Pin prometheus version to 2.x for now
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * More fixes for versions
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Start adding loki to the auth test
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    ---------
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -11,13 +11,12 @@ usage() {
   echo ""
   echo "Runs an integration test"
   echo ""
-  echo "  <test-dir>           - The test directory. Expects these files:"
-  echo "    cluster.yaml       - The Kind cluster config"
-  echo "    test-manifest.yaml - The test manifest, which defines test prerequisites"
-  echo "    values.yaml        - The k8s-monitoring Helm chart values file to use"
-  echo "    test-values.yaml   - The k8s-monitoring-test Helm chart values file to use, which defines test queries"
+  echo "  <test-dir>           - The test directory. Expects this file:"
+  echo "    test-manifest.yaml - The test manifest, which defines cluster, and deployments"
 }
 
+CREATE_CLUSTER=${CREATE_CLUSTER:-true}
+HEADLESS=${HEADLESS:-false}
 TEST_DIRECTORY=$1
 if [ -z "${TEST_DIRECTORY}" ]; then
   echo "Test directory not defined!"
@@ -25,104 +24,97 @@ if [ -z "${TEST_DIRECTORY}" ]; then
   exit 1
 fi
 
-clusterConfig="${TEST_DIRECTORY}/cluster.yaml"
 testManifest="${TEST_DIRECTORY}/test-manifest.yaml"
-valuesFile="${TEST_DIRECTORY}/values.yaml"
-testValuesFile="${TEST_DIRECTORY}/test-values.yaml"
-
 if [ ! -f "${testManifest}" ]; then
   echo "${testManifest} does not exist!"
   usage
   exit 1
 fi
 
-if [ ! -f "${valuesFile}" ]; then
-  echo "${valuesFile} does not exist!"
-  usage
-  exit 1
-fi
-
-
 set -eo pipefail  # Exit immediately if a command fails.
 
-clusterName=$(yq -r .cluster.name "${valuesFile}")
+#
+# Cluster creation
+#
+if [ "${CREATE_CLUSTER}" == "true" ]; then
+  clusterType=$(yq -r ".cluster.type // \"kind\"" "${testManifest}")
+  clusterName=$(yq -r ".cluster.name // \"$(basename "${TEST_DIRECTORY}")\"" "${testManifest}")
+  clusterConfig=$(yq -r ".cluster.config // \"\"" "${testManifest}")
 
-DEPLOY_GRAFANA=${DEPLOY_GRAFANA:-true}
-DELETE_CLUSTER=${DELETE_CLUSTER:-false}
-cleanup() {
-  helm ls -A || true
-
-  if [ "${DELETE_CLUSTER}" == "true" ]; then
-    kind delete cluster --name "${clusterName}" || true
-  fi
-}
-trap cleanup EXIT
-
-if ! kind get clusters | grep -q "${clusterName}"; then
-  echo "Creating cluster..."
-  if [ ! -f "${clusterConfig}" ]; then
-    kind create cluster --name "${clusterName}"
+  if [ "${clusterType}" == "kind" ]; then
+    if ! kind get clusters | grep -q "${clusterName}"; then
+      if [ ! -f "${clusterConfig}" ]; then
+        kind create cluster --name "${clusterName}"
+      else
+        kind create cluster --name "${clusterName}" --config "${TEST_DIRECTORY}/${clusterConfig}"
+      fi
+    fi
   else
-    kind create cluster --name "${clusterName}" --config "${clusterConfig}"
+    echo "Unknown cluster type: \"${clusterType}\""
   fi
 fi
 
-prerequisiteCount=$(yq -r '.prerequisites | length' "${testManifest}")
-for ((i=0; i<prerequisiteCount; i++)); do
-  prereqName=$(yq -r .prerequisites[$i].name "${testManifest}")
-  if [ "${prereqName}" == "grafana" ] && [ "${DEPLOY_GRAFANA}" == "false" ]; then
+#
+# Deployments
+#
+deploymentCount=$(yq -r '.deployments | length' "${testManifest}")
+for ((i=0; i<deploymentCount; i++)); do
+  name=$(yq -r .deployments[$i].name "${testManifest}")
+  type=$(yq -r .deployments[$i].type "${testManifest}")
+  skipOnHeadless=$(yq -r ".deployments[$i].skipOnHeadless // \"false\"" "${testManifest}")
+
+  if [ "${HEADLESS}" == "true" ] && [ "${skipOnHeadless}" == "true" ]; then
     continue
   fi
-  echo "Deploying ${prereqName}..."
+  echo "Deploying ${name}..."
 
-  prereqType=$(yq -r .prerequisites[$i].type "${testManifest}")
-  namespace=$(yq -r ".prerequisites[$i].namespace // \"\"" "${testManifest}")
-  if [ -n "${namespace}" ]; then
-    namespaceArg="--namespace ${namespace}"
-  fi
+  namespace=$(yq -r ".deployments[$i].namespace // \"\"" "${testManifest}")
+  if [ -n "${namespace}" ]; then namespaceArg="--namespace ${namespace}"; else namespaceArg=""; fi
 
-  if [ "${prereqType}" == "manifest" ]; then
-    prereqUrl=$(yq -r ".prerequisites[$i].url // \"\"" "${testManifest}")
-    prereqFile=$(yq -r ".prerequisites[$i].file // \"\"" "${testManifest}")
-    if [ -n "${prereqUrl}" ]; then
-      kubectl apply -f "${prereqUrl}" ${namespaceArg}
-    elif [ -n "${prereqFile}" ]; then
-      envsubst < "${PARENT_DIR}/${prereqFile}" | kubectl apply ${namespaceArg} -f -
+  if [ "${type}" == "manifest" ]; then
+    manifestURL=$(yq -r ".deployments[$i].url // \"\"" "${testManifest}")
+    manifestFile=$(yq -r ".deployments[$i].file // \"\"" "${testManifest}")
+    if [ -n "${manifestURL}" ]; then
+      kubectl apply -f "${manifestURL}" ${namespaceArg}
+    elif [ -n "${manifestFile}" ]; then
+      envsubst < "${TEST_DIRECTORY}/${manifestFile}" | kubectl apply ${namespaceArg} -f -
     else
-      echo "No URL or file specified for manifest prerequisite"
+      echo "No URL or file specified for manifest deployment \"${name}\""
       exit 1
     fi
-  elif [ "${prereqType}" == "helm" ]; then
-    prereqRepo=$(yq -r ".prerequisites[$i].repo // \"\"" "${testManifest}")
-    prereqRepoArg=""
-    if [ -n "${prereqRepo}" ]; then prereqRepoArg="--repo ${prereqRepo}"; fi
-    prereqChart=$(yq -r .prerequisites[$i].chart "${testManifest}")
-    prereqVersion=$(yq -r ".prerequisites[$i].version // \"\"" "${testManifest}")
-    prereqVersionArg=""
-    if [ -n "${prereqVersion}" ]; then prereqVersionArg="--version ${prereqVersion}"; fi
-    prereqValues="$(yq -r ".prerequisites[$i].values // \"\"" "${testManifest}")"
-    prereqValuesFile="$(yq -r ".prerequisites[$i].valuesFile // \"\"" "${testManifest}")"
 
-    if [ -n "${prereqValuesFile}" ]; then
-      helm upgrade --install "${prereqName}" ${namespaceArg} --create-namespace ${prereqRepoArg} "${prereqChart}" ${prereqVersionArg} -f "${PARENT_DIR}/${prereqValuesFile}" --hide-notes --wait
-    elif [ -n "${prereqChart}" ]; then
-      echo "${prereqValues}" > temp-values.yaml
-      helm upgrade --install "${prereqName}" ${namespaceArg} --create-namespace ${prereqRepoArg} "${prereqChart}" ${prereqVersionArg} -f temp-values.yaml --hide-notes --wait
+  elif [ "${type}" == "helm" ]; then
+    helmRepo=$(yq -r ".deployments[$i].repo // \"\"" "${testManifest}")
+    helmRepoArg=""
+    if [ -n "${helmRepo}" ]; then helmRepoArg="--repo ${helmRepo}"; else helmRepoArg=""; fi
+    helmChart=$(yq -r ".deployments[$i].chart // \"\"" "${testManifest}")
+    helmChartPath=$(yq -r ".deployments[$i].chartPath // \"\""  "${testManifest}")
+    version=$(yq -r ".deployments[$i].version // \"\"" "${testManifest}")
+    versionArg=""
+    if [ -n "${version}" ]; then versionArg="--version ${version}"; else versionArg=""; fi
+    helmValues="$(yq -r ".deployments[$i].values // \"\"" "${testManifest}")"
+    helmValuesFile="$(yq -r ".deployments[$i].valuesFile // \"\"" "${testManifest}")"
+    helmTest="$(yq -r ".deployments[$i].test // \"false\"" "${testManifest}")"
+
+    if [ -n "${helmChartPath}" ]; then
+      helmChart="$(cd "$(dirname "${PARENT_DIR}/${helmChartPath}")" && pwd)/$(basename "${helmChartPath}")"
+    fi
+
+    if [ -n "${helmValuesFile}" ]; then
+      helm upgrade --install "${name}" ${namespaceArg} --create-namespace ${helmRepoArg} "${helmChart}" ${versionArg} -f "${TEST_DIRECTORY}/${helmValuesFile}" --hide-notes --wait
+    elif [ -n "${helmChart}" ]; then
+      echo "${helmValues}" > temp-values.yaml
+      helm upgrade --install "${name}" ${namespaceArg} --create-namespace ${helmRepoArg} "${helmChart}" ${versionArg} -f temp-values.yaml --hide-notes --wait
       rm temp-values.yaml
     else
-      helm upgrade --install "${prereqName}" ${namespaceArg} --create-namespace --repo "${prereqRepo}" "${prereqChart}" ${prereqVersionArg} --hide-notes --wait
+      helm upgrade --install "${name}" ${namespaceArg} --create-namespace --repo "${helmRepo}" "${helmChart}" ${versionArg} --hide-notes --wait
+    fi
+
+    if [ "${helmTest}" == "true" ]; then
+      helm test "${name}" ${namespaceArg} --logs
     fi
   else
-    echo "Unknown prerequisite type: ${prereqType}"
+    echo "Unknown deployment type: ${type}"
     exit 1
   fi
 done
-
-echo "Deploying chart..."
-helm upgrade --install k8smon "${PARENT_DIR}/charts/k8s-monitoring" -f "${valuesFile}" --wait
-
-if [ -f "${testValuesFile}" ]; then
-  echo "Deploying test chart..."
-  helm upgrade --install k8smon-test "${PARENT_DIR}/charts/k8s-monitoring-test" -f <(envsubst < "${testValuesFile}") --wait
-  helm test k8smon-test --logs
-fi
