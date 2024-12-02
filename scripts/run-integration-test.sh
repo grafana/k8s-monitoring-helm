380e90214435bf8d622966131205d7af05ab10e9
Pete Wall <pete.wall@grafana.com>
2024-12-02 16:14:32 -0600

    Finish converting all integration and platform tests to flux
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- scripts/run-integration-test.sh
+++ scripts/run-integration-test.sh
@@ -24,97 +24,40 @@ if [ -z "${TEST_DIRECTORY}" ]; then
   exit 1
 fi
 
-testManifest="${TEST_DIRECTORY}/test-manifest.yaml"
-if [ ! -f "${testManifest}" ]; then
-  echo "${testManifest} does not exist!"
-  usage
-  exit 1
-fi
-
 set -eo pipefail  # Exit immediately if a command fails.
 
 #
 # Cluster creation
 #
 if [ "${CREATE_CLUSTER}" == "true" ]; then
-  clusterType=$(yq -r ".cluster.type // \"kind\"" "${testManifest}")
-  clusterName=$(yq -r ".cluster.name // \"$(basename "${TEST_DIRECTORY}")\"" "${testManifest}")
-  clusterConfig=$(yq -r ".cluster.config // \"\"" "${testManifest}")
-
-  if [ "${clusterType}" == "kind" ]; then
+  clusterName=$(yq eval '.cluster.name' "${TEST_DIRECTORY}/values.yaml")
+  if [ -f "${TEST_DIRECTORY}/kind-cluster-config.yaml" ]; then
     if ! kind get clusters | grep -q "${clusterName}"; then
-      if [ ! -f "${clusterConfig}" ]; then
-        kind create cluster --name "${clusterName}"
-      else
-        kind create cluster --name "${clusterName}" --config "${TEST_DIRECTORY}/${clusterConfig}"
-      fi
+      kind create cluster --name "${clusterName}" --config "${TEST_DIRECTORY}/kind-cluster-config.yaml"
     fi
+  elif [ -f "${TEST_DIRECTORY}/gke-cluster-config.yaml" ]; then
+    clusterType="gke"
+    clusterConfig="${TEST_DIRECTORY}/gke-cluster-config.yaml"
+    echo "gcloud cluster creation not yet implemented"
   else
-    echo "Unknown cluster type: \"${clusterType}\""
+    if ! kind get clusters | grep -q "${clusterName}"; then
+      kind create cluster --name "${clusterName}"
+    fi
   fi
 fi
 
-#
-# Deployments
-#
-deploymentCount=$(yq -r '.deployments | length' "${testManifest}")
-for ((i=0; i<deploymentCount; i++)); do
-  name=$(yq -r .deployments[$i].name "${testManifest}")
-  type=$(yq -r .deployments[$i].type "${testManifest}")
-  skipOnHeadless=$(yq -r ".deployments[$i].skipOnHeadless // \"false\"" "${testManifest}")
+# Deploy flux
+if command -v flux &> /dev/null; then
+  flux install
+else
+  helm upgrade --install --namespace flux-system --create-namespace flux oci://ghcr.io/fluxcd-community/charts/flux2 --wait
+fi
 
-  if [ "${HEADLESS}" == "true" ] && [ "${skipOnHeadless}" == "true" ]; then
-    continue
-  fi
-  echo "Deploying ${name}..."
+# Deploy prerequisites
+kubectl apply -f ${TEST_DIRECTORY}/deployments
 
-  namespace=$(yq -r ".deployments[$i].namespace // \"\"" "${testManifest}")
-  if [ -n "${namespace}" ]; then namespaceArg="--namespace ${namespace}"; else namespaceArg=""; fi
+# Deploy k8s-monitoring
+helm upgrade --install k8smon ${PARENT_DIR}/charts/k8s-monitoring -f ${TEST_DIRECTORY}/values.yaml --wait
 
-  if [ "${type}" == "manifest" ]; then
-    manifestURL=$(yq -r ".deployments[$i].url // \"\"" "${testManifest}")
-    manifestFile=$(yq -r ".deployments[$i].file // \"\"" "${testManifest}")
-    if [ -n "${manifestURL}" ]; then
-      kubectl apply -f "${manifestURL}" ${namespaceArg}
-    elif [ -n "${manifestFile}" ]; then
-      envsubst < "${TEST_DIRECTORY}/${manifestFile}" | kubectl apply ${namespaceArg} -f -
-    else
-      echo "No URL or file specified for manifest deployment \"${name}\""
-      exit 1
-    fi
-
-  elif [ "${type}" == "helm" ]; then
-    helmRepo=$(yq -r ".deployments[$i].repo // \"\"" "${testManifest}")
-    helmRepoArg=""
-    if [ -n "${helmRepo}" ]; then helmRepoArg="--repo ${helmRepo}"; else helmRepoArg=""; fi
-    helmChart=$(yq -r ".deployments[$i].chart // \"\"" "${testManifest}")
-    helmChartPath=$(yq -r ".deployments[$i].chartPath // \"\""  "${testManifest}")
-    version=$(yq -r ".deployments[$i].version // \"\"" "${testManifest}")
-    versionArg=""
-    if [ -n "${version}" ]; then versionArg="--version ${version}"; else versionArg=""; fi
-    helmValues="$(yq -r ".deployments[$i].values // \"\"" "${testManifest}")"
-    helmValuesFile="$(yq -r ".deployments[$i].valuesFile // \"\"" "${testManifest}")"
-    helmTest="$(yq -r ".deployments[$i].test // \"false\"" "${testManifest}")"
-
-    if [ -n "${helmChartPath}" ]; then
-      helmChart="$(cd "$(dirname "${PARENT_DIR}/${helmChartPath}")" && pwd)/$(basename "${helmChartPath}")"
-    fi
-
-    if [ -n "${helmValuesFile}" ]; then
-      helm upgrade --install "${name}" ${namespaceArg} --create-namespace ${helmRepoArg} "${helmChart}" ${versionArg} -f "${TEST_DIRECTORY}/${helmValuesFile}" --hide-notes --wait
-    elif [ -n "${helmChart}" ]; then
-      echo "${helmValues}" > temp-values.yaml
-      helm upgrade --install "${name}" ${namespaceArg} --create-namespace ${helmRepoArg} "${helmChart}" ${versionArg} -f temp-values.yaml --hide-notes --wait
-      rm temp-values.yaml
-    else
-      helm upgrade --install "${name}" ${namespaceArg} --create-namespace --repo "${helmRepo}" "${helmChart}" ${versionArg} --hide-notes --wait
-    fi
-
-    if [ "${helmTest}" == "true" ]; then
-      helm test "${name}" ${namespaceArg} --logs
-    fi
-  else
-    echo "Unknown deployment type: ${type}"
-    exit 1
-  fi
-done
+# Run tests
+helm test k8s-monitoring-test
