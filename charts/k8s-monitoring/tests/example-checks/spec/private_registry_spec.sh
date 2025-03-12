9c740fa31de812215a36934951153446700310b5
Pete Wall <pete.wall@grafana.com>
2025-03-12 14:03:20 -0500

    Minor fixes: * Fix copy-paste error that put "cadvisor" in nearly all cluster metric sources * Fix missing registry overrides for configreloader example * Fix typo in private registry example check and make sure both examples are checked
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- charts/k8s-monitoring/tests/example-checks/spec/private_registry_spec.sh
+++ charts/k8s-monitoring/tests/example-checks/spec/private_registry_spec.sh
@@ -1,11 +1,12 @@
-findUnmodifiedImageRegisries() {
+findUnmodifiedImageRegistries() {
   grep "image: " ../../docs/examples/private-image-registries/globally/output.yaml | grep -v "my.registry.com"
+  grep "image: " ../../docs/examples/private-image-registries/individual/output.yaml | grep -v "my.registry.com"
 }
 
 Describe 'Private Registry Check'
   Describe 'Using private registry'
     It 'does not contain any image references not in the image registry'
-      When call findUnmodifiedImageRegisries
+      When call findUnmodifiedImageRegistries
       The status should be failure
       The output should equal ''
     End
