92fee57b900489d3a4efba5cd4280cc8ec7ebe9a
Pete Wall <pete.wall@grafana.com>
2025-05-20 15:50:57 -0500

    Update to Alloy Operator 0.2.8
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

---
+++ charts/k8s-monitoring/tests/misc-checks/spec/crd-link_spec.sh
@@ -0,0 +1,11 @@
+Describe 'Embedded Secrets Check'
+  Describe 'Using secret.embed = true'
+    It 'does not create any Kubernetes Secrets'
+      When call wget --spider --server-response https://github.com/grafana/alloy-operator/releases/download/v${ALLOY_OPERATOR_VERSION}/collectors.grafana.com_alloy.yaml
+      The status should be success
+      The stderr should include 'HTTP/1.1 200 OK'
+      The stderr should include 'Content-Type: application/octet-stream'
+      The stderr should include 'remote file exists'
+    End
+  End
+End
