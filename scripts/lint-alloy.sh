88a11dce950ebd97eeb1c965e16d9290b5668297
Pete Wall <pete.wall@grafana.com>
2025-08-06 14:33:27 -0500

    Add Helm Chart Toolbox (#1762)
    * Update unit tests, integration tests, platform tests, and generator tools to the Helm Chart Toolbox
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Update generated files, update platform test action, fix yaml lint
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Fix platform test list tests
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Need to run make all before running the test
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Need to run make inside the directory
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Convert OpenShift platform test
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Fix yaml lint errors
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Update Loki
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Finish converting tests and elminating extra scripts
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    * Update generated file
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>
    
    ---------
    
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- scripts/lint-alloy.sh
+++ scripts/lint-alloy.sh
@@ -1,61 +1,36 @@
 #!/usr/bin/env bash
-PARENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source "${PARENT_DIR}/scripts/includes/utils.sh"
-source "${PARENT_DIR}/scripts/includes/logging.sh"
 
 usage() {
   echo "USAGE: lint-alloy.sh [--public-preview] output.alloy [output2.alloy...]"
   echo ""
   echo "Uses Grafana Alloy to lint the generated configuration"
-  echo "  --public-preview    - Switch to the public-preview stability level"
 }
 
 # check to see if alloy is installed
 if [[ "$(command -v alloy || true)" = "" ]]; then
-  emergency "alloy is required if running lint locally, see: (https://grafana.com/docs/alloy/latest/) or run: brew install grafana-alloy";
+  echo "Error: alloy is required if running lint locally, see: (https://grafana.com/docs/alloy/latest/) or run: brew install grafana-alloy";
+  exit 1
 fi
-
-# determine whether or not the script is called directly or sourced
-(return 0 2>/dev/null) && sourced=1 || sourced=0
 
 # Initialize a flag variable
-FORMAT="console"
 STABILITY_LEVEL=generally-available
 
-# Loop through all arguments
-for arg in "$@"
-do
-  if [[ "${arg}" == "--format=checkstyle" ]]; then
-    # Set the flag to true if the specific argument is found
-    FORMAT="checkstyle"
-    break
-  fi
-done
-
-if [[ "${FORMAT}" == "console" ]]; then
-  # output the heading
-  heading "Kubernetes Monitoring Helm" "Performing Alloy Lint"
-fi
-
 statusCode=0
-checkstyle='<?xml version="1.0" encoding="utf-8"?><checkstyle version="4.3">'
 
 # Inject a component that utilizes Kubernetes discovery, so we know that the config will fail in a predictable way.
 k8sDiscovery='discovery.kubernetes "lint_config_component" { role = "nodes" }'
 
 for file in "$@";
 do
-  if grep "${file}" -e "otelcol.receiver.filelog" >/dev/null; then
-    STABILITY_LEVEL=public-preview
-  fi
-
   # if the file doesn't exist skip it
   if [[ ! -f "${file}" ]]; then
    continue
   fi
 
-  # add file to checkstyle output
-  checkstyle="${checkstyle}<file name=\"${file}\">"
+  if grep "${file}" -e "otelcol.receiver.filelog" >/dev/null; then
+    STABILITY_LEVEL=public-preview
+  fi
+
   fmt_output=$(alloy fmt "${file}" 2>&1)
   fmtCode="$?"
   fmt_output=$(echo "${fmt_output}" | grep -v "Error: encountered errors during formatting")
@@ -74,30 +49,15 @@ do
 
   # if the current code is 0, output the file name for logging purposes
   if [[ "${fmtCode}" == 0 ]] && [[ "${run_code}" == 0 ]]; then
-    # output to console only if the format is console
-    if [[ "${FORMAT}" == "console" ]]; then
-      echo -e "\\x1b[32m${file}\\x1b[0m: no issues found"
-    fi
-    checkstyle="${checkstyle}</file>"
+    echo -e "\\x1b[32m${file}\\x1b[0m: no issues found"
   else
-    # output to console only if the format is console
-    if [[ "${FORMAT}" == "console" ]]; then
-      echo -e "\\x1b[31m${file}\\x1b[0m: issues found"
-    fi
+    echo -e "\\x1b[31m${file}\\x1b[0m: issues found"
 
     # output alloy fmt errors
     if [[ "${fmtCode}" != 0 ]]; then
       # loop each found issue
       while IFS= read -r row; do
-        # Process each line here
-        line=$(echo "${row}" | awk -F ':' '{print $2}')
-        column=$(echo "${row}" | awk -F ':' '{print $3}')
-        error=$(echo "${row}" | cut -d':' -f4- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | sed -e 's/"/\&quot;/g' | xargs || true)
-        checkstyle="${checkstyle}<error line=\"${line}\" column=\"${column}\" severity=\"error\" message=\"${error}\" source=\"alloy\"/>"
-        # output to console only if the format is console
-        if [[ "${FORMAT}" == "console" ]]; then
-          echo "  - ${row}"
-        fi
+        echo "  - ${row}"
       done <<< "${fmt_output}"
     fi
 
@@ -106,39 +66,13 @@ do
       # loop each found issue
       while IFS= read -r row; do
         if [[ "${row}" =~ "Error: " ]]; then
-          # Process each line here
-          line=$(echo "${row}" | awk -F ':' '{print $2}')
-          column=$(echo "${row}" | awk -F ':' '{print $3}')
-          error=$(echo "${row}" | cut -d':' -f5- | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | sed -e 's/"/\&quot;/g' | xargs || true)
-          checkstyle="${checkstyle}<error line=\"${line}\" column=\"${column}\" severity=\"error\" message=\"${error}\" source=\"alloy\"/>"
-          # output to console only if the format is console
-          if [[ "${FORMAT}" == "console" ]]; then
-            echo "  - ${row}"
-          fi
+          echo "  - ${row}"
         fi
       done <<< "${run_output}"
     fi
-
-    checkstyle="${checkstyle}</file>"
 
     if [[ "${statusCode}" == 0 ]]; then
       statusCode=1
     fi
   fi
 done
-
-checkstyle="${checkstyle}</checkstyle>"
-
-if [[ "${FORMAT}" == "checkstyle" ]]; then
-  echo -n "${checkstyle}"
-else
-  echo ""
-  echo ""
-fi
-
-# if the script was called by another, send a valid exit code
-if [[ "${sourced}" == "1" ]]; then
-  return "${statusCode}"
-else
-  exit "${statusCode}"
-fi
